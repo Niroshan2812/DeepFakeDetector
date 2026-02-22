0bdff3d980246713bd34b0063cdd33427ddc095a
Niroshan <niroshan9812@gmail.com>
2026-02-22 11:04:59 +0530

    BasicStructure for initial Start

---
+++ CenterDeepFacke/src/test/java/com/intelligent/centerdeepfacke/CenterDeepFackeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intelligent.centerdeepfacke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CenterDeepFackeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
